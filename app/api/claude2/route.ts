2ddef39d3954bcd7793e249a4df1632495685a99
ikigAI <ikigailabs@proton.me>
2023-08-27 22:00:40 +0200

    add claud2 + anthropics sdk

---
+++ app/api/claude2/route.ts
@@ -0,0 +1,17 @@
+import Anthropic from '@anthropic-ai/sdk';
+
+const client = new Anthropic(); // gets API Key from environment variable ANTHROPIC_API_KEY
+
+async function main() {
+  const result = await client.completions.create({
+    prompt: `${Anthropic.HUMAN_PROMPT} how does a court case get to the Supreme Court? ${Anthropic.AI_PROMPT}`,
+    model: 'claude-2',
+    max_tokens_to_sample: 300,
+  });
+  console.log(result.completion);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
