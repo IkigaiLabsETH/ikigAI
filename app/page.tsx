f14ea7a733cb9bd1f5054446f0c7c421cfff49c9
ikigAI <ikigailabs@proton.me>
2023-08-28 16:39:09 +0200

    add filmlover

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import HomePage from "@/templates/HomePage";
 import PineconePage from "@/templates/PineConePage";
+import Filmlover from "./movies/page";
 
 const Home: NextPage = () => {
     return (
         <>
         <HomePage />
         <PineconePage />
+        <Filmlover />
     </>
     );
 };
