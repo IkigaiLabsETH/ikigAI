3b7fd5a028b58aba42fbd46d1e5c68f879b706d2
ikigAI <ikigailabs@proton.me>
2023-08-28 14:01:28 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      "functions": {
+        "pages/**/*": {
+          "excludeFiles": "public/**"
+        }
+      },
     experimental: {
       legacyBrowsers: false,
       outputFileTracingIgnores: ['**canvas**'],
